306cf7245c7c91d525ecbc470c52f54f1d90749e
Pacool84 <flopez_1384@hotmail.com>
2024-10-13 10:48:39 -0600

    [ADD] Ejercicio de sets

---
+++ ejercicio_01.py
@@ -0,0 +1,23 @@
+'''
+Para resolver este desafío, debes escribir un algoritmo que elimine los elementos repetidos para obtener un conjunto único llamado new_set.
+
+Este algoritmo recibirá como entrada cuatro conjuntos de países, estos países serán de todo el continente americano divididos de la siguiente manera:
+
+countries - Países del continente en general.
+northAmerica - Países del norte de América.
+centralAmerica - Países del centro de América.
+southAmerica - Países del sur de América.
+En resumen, el algoritmo deberá eliminar los elementos repetidos de los cuatro conjuntos de países y obtener un conjunto único llamado new_set.
+
+print(new_set)
+
+'''
+countries = {"MX", "COL", "ARG", "USA"}
+northAm = {"USA", "CANADA"}
+centralAm = {"MX", "GT", "BZ"}
+southAm = {"COL", "BZ", "ARG"}
+
+# Escribe tu solución 👇
+new_set = countries | northAm | centralAm | southAm
+
+print(new_set)
