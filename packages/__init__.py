bbbcd0491653cc35ea429790c508e41fafc896f5
Pacool84 <flopez_1384@hotmail.com>
2024-11-18 14:26:11 -0600

    [ADD] Clase de paquetes con los conceptos de main.py e __init_.py

---
+++ packages/__init__.py
@@ -0,0 +1,11 @@
+#Este archivo se debe de utilizar para la utilizacion de paquetes
+#Se utiliza para versiones de python antes de la 3.3
+#Tambien este archivo se utiliza para inicializar alguna variable o importaciones a este paquete 
+
+print('Se inicio paquete')
+
+URL = 'www.platzi.com'
+
+#El archivo __init__.py tambien se utiliza para definir NAMESPACE
+
+import packages.mod_1, packages.mod_2 #Creamos un NAMESPACE
